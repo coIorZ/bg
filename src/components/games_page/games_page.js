6ac75e62155d712e3bb7212e95df0ed75dba7019
coiorz <coiorz1989@gmail.com>
2016-11-01 10:18:57 +0800

    fix games page motion throttle bug

--- src/components/games_page/games_page.js
+++ src/components/games_page/games_page.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { Motion, spring } from 'react-motion';
 import { throttle } from 'lodash';
 
+
 import styles from './games_page.css';
 
 class GamesPage extends Component {
@@ -12,22 +13,27 @@ class GamesPage extends Component {
 		this.state = {
 			y: 0
 		};
+		this.handleWheel = this.handleWheel.bind(this);
 	}
 
 	render() {
 		const height = this.props.clientHeight;
 		return (
 			<div>
-				<Motion style={{y: spring(this.state.y, 200)}}>
+				<Motion style={{y: spring(this.state.y)}}>
 					{({ y }) => 
 						<div style={{ 
 								height,
 								transform: `translate3d(0, ${y}px, 0)`
 							}}
-							onWheel={throttle(this.handleWheel.bind(this), 100)}>
+							onWheel={throttle(this.handleWheel, 100)}>
 							<div className={styles.game + ' ' + styles.tnl}>
 							</div>
 							<div className={styles.game + ' ' + styles.coup}>
+							</div>
+							<div className={styles.game + ' ' + styles.orange}>
+							</div>
+							<div className={styles.game + ' ' + styles.blue}>
 							</div>
 						</div>
 					}
@@ -37,16 +43,16 @@ class GamesPage extends Component {
 	}
 
 	handleWheel(e) {
-		if(e.deltaY == 0) return;
 		const h = this.props.clientHeight;
 		let y = this.state.y;
-		y += e.deltaY * -1;
+		y += e.deltaY * -1.5;
 		if(y >= 0) y = 0;
+		if(y <= -1 * h * 3) y = -1 * h * 3;
 		this.setState({ y });
 		window.clearTimeout(this._timer);
 		this._timer = window.setTimeout(() => {
 			const n = y / h | 0;
-			y = h * (Math.abs(y % h) >= h * (e.deltaY > 0 ? 1/3 : 2/3) ? n - 1 : n);
+			y = h * (Math.abs(y % h) >= h / 2 ? n - 1 : n);
 			this.setState({ y });
 		},120);
 	}
